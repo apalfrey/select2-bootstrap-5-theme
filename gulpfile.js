028bbad9e98e304642b27018617f1860100f63f4
Andrew Palfrey <apalfrey@apalfrey.me>
2021-02-25 13:09:03 +0000

    Added linting

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,14 @@
 const gulp = require( "gulp" )
 
 // External dependencies
+const del = require( "del" )
 const gulpif = require( "gulp-if" )
-const plumber = require( "gulp-plumber" )
 const rename = require( "gulp-rename" )
 const sass = require( "gulp-sass" )
+const stylelint = require( "gulp-stylelint" )
 
 const compile = ( style ) => {
     return gulp.src( "src/select2-bootstrap-5-theme.scss" )
-        .pipe( plumber() )
         .pipe( sass( {
             precision: 6,
             outputStyle: style,
@@ -28,11 +28,27 @@ gulp.task( "compile:min", () => {
     return compile( "compressed" )
 } )
 
+gulp.task( "lint", () => {
+    return gulp.src( "src/**/*.scss" )
+        .pipe( stylelint( {
+            failAfterError: true,
+            reporters: [
+                { formatter: 'verbose', console: true },
+            ]
+        } ) )
+} )
+
 gulp.task( "watch", ( done ) => {
     gulp.watch( "src/*.scss", gulp.series( "compile" ) )
 
     done()
 } )
 
-gulp.task( "compile", gulp.series( "compile:dev", "compile:min" ) )
+gulp.task( "clean", () => {
+    return del( [
+        "dist"
+    ] )
+} )
+
+gulp.task( "compile", gulp.series( "clean", "lint", "compile:dev", "compile:min" ) )
 gulp.task( "default", gulp.series( "compile", "watch" ) )
