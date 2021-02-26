2e25a6d2ebe52859a480e38e3a1c0c0a0ba896f5
Andrew Palfrey <apalfrey@apalfrey.me>
2021-02-26 00:55:31 +0000

    Added select2 resizing to examples

--- docs/script.js
+++ docs/script.js
@@ -1,4 +1,4 @@
-$( function () {
+function select2( size ) {
     $( "select" ).each( function () {
         $( this ).select2( {
             theme: "bootstrap-5",
@@ -6,6 +6,24 @@ $( function () {
             placeholder: $( this ).data( "placeholder" ),
             allowClear: Boolean( $( this ).data( "allow-clear" ) ),
             closeOnSelect: !$( this ).attr( "multiple" ),
+            selectionCssClass: size == "small" || size == "large" ? "select2--" + size : ""
         } );
     } );
-} );
+}
+
+select2()
+
+var buttons = document.querySelectorAll(".select2-size")
+
+buttons.forEach( function( button ) {
+    var id = button.id
+    button.addEventListener( "click", function( e ) {
+        e.preventDefault()
+        select2( id )
+        document.querySelectorAll(".select2-size").forEach( function( item ) {
+            item.classList.remove( "active" )
+        } )
+
+        this.classList.add( "active" )
+    } )
+} )
