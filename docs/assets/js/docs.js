0df270aeacba966da5282ef9205b25abc72e44ba
Andrew Palfrey <apalfrey@apalfrey.me>
2021-05-26 01:57:12 +0100

    Implemented anchor.js functionality

---
+++ docs/assets/js/docs.js
@@ -0,0 +1,6 @@
+window.addEventListener( 'DOMContentLoaded', function () {
+    anchors.options = {
+        icon: '#'
+    }
+    anchors.add('.s2bs5-content > h2, .s2bs5-content > h3, .s2bs5-content > h4, .s2bs5-content > h5')
+} )
